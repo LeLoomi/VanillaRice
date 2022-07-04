2eed82bb0edeffb87cf10dc7321994349599e146
LeLoomi <lohr.eliah@gmail.com>
2022-07-04 23:58:12 +0200

    items and crafting added, placeholder texture atm

--- src/main/java/net/leloomi/vanillarice/VanillaRice.java
+++ src/main/java/net/leloomi/vanillarice/VanillaRice.java
@@ -1,18 +1,21 @@
 package net.leloomi.vanillarice;
 
 import net.fabricmc.api.ModInitializer;
+import net.leloomi.vanillarice.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class VanillaRice implements ModInitializer
 {
-	public static final String MODID = "vanillarice";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+	public static final String MOD_ID = "vanillarice";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 
 	@Override
 	public void onInitialize()
 	{
+		ModItems.registerModItems();
 
+		VanillaRice.LOGGER.debug(VanillaRice.MOD_ID + ": Initialization done!");
 	}
 }
---
+++ src/main/java/net/leloomi/vanillarice/item/ModItems.java
@@ -0,0 +1,42 @@
+package net.leloomi.vanillarice.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.leloomi.vanillarice.VanillaRice;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModItems
+{
+    public static final Item RICE_SEEDS = registerItem("rice_seeds",
+            new Item(new FabricItemSettings().group(ModItemGroup.RICE)));
+    
+    public static final Item RICE_GRAIN = registerItem("rice_grain",
+            new Item(new FabricItemSettings().group(ModItemGroup.RICE)));
+
+    public static final Item RICE_BOWL = registerItem("rice_bowl",
+            new Item(new FabricItemSettings().group(ModItemGroup.RICE)));
+
+    public static final Item MAKI_SUSHI = registerItem("maki_sushi",
+            new Item(new FabricItemSettings().group(ModItemGroup.RICE)));
+
+    public static final Item FRIED_RICE = registerItem("fried_rice",
+            new Item(new FabricItemSettings().group(ModItemGroup.RICE)));
+
+    public static final Item MOCHI = registerItem("mochi",
+            new Item(new FabricItemSettings().group(ModItemGroup.RICE)));
+
+    public static final Item RICE_BAG = registerItem("rice_bag",
+            new Item(new FabricItemSettings().group(ModItemGroup.RICE)));
+
+
+    private static Item registerItem(String name, Item item)
+    {
+        return Registry.register(Registry.ITEM, new Identifier(VanillaRice.MOD_ID, name), item);
+    }
+
+    public static void registerModItems()
+    {
+        VanillaRice.LOGGER.debug(VanillaRice.MOD_ID + ": Registering Items...");
+    }
+}
