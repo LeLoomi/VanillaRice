95d917a5195d786944b3f5295d26e89b41211a15
LeLoomi <lohr.eliah@gmail.com>
2024-02-08 00:25:24 +0100

    Moved items to corresponding vanilla item groups; updated the seeds texture to be less realistic but way prettier :)

--- src/main/java/net/leloomi/vanillarice/item/ModItemGroup.java
+++ src/main/java/net/leloomi/vanillarice/item/ModItemGroup.java
@@ -1,34 +1,22 @@
 package net.leloomi.vanillarice.item;
 
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
-import net.leloomi.vanillarice.VanillaRice;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroups;
 
 public class ModItemGroup
 {
-    public static final ItemGroup RICE = Registry.register(
-            Registries.ITEM_GROUP,
-            new Identifier(VanillaRice.MOD_ID, "rice"),
-            FabricItemGroup.builder()
-                    .displayName(Text.translatable("itemGroup.vanillarice"))
-                    .icon( () -> new ItemStack(ModItems.FRIED_RICE))
-                    .entries((displayContext, entries) -> {
-                        entries.add(ModItems.RICE_SEEDS);
-                        entries.add(ModItems.RICE_GRAIN);
-                        entries.add(ModItems.RICE_BAG);
-                        entries.add(ModItems.RICE_BOWL);
-                        entries.add(ModItems.FRIED_RICE);
-                        entries.add(ModItems.MAKI_SUSHI);
-                        entries.add(ModItems.MOCHI);
-
-                    }).build());
-
     public static void registerItemGroups() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(entries -> {
+            entries.add(ModItems.RICE_SEEDS);
+            entries.add(ModItems.RICE_GRAIN);
+            entries.add(ModItems.RICE_BAG);
+        });
 
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> {
+            entries.add(ModItems.RICE_BOWL);
+            entries.add(ModItems.FRIED_RICE);
+            entries.add(ModItems.MAKI_SUSHI);
+            entries.add(ModItems.MOCHI);
+        });
     }
 }
