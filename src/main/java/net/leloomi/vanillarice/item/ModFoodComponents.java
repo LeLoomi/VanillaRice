4b3125faa0e72b29ce16dc24cee18c9056866cf0
Eliah Lohr <lohr.eliah@gmail.com>
2025-08-11 15:45:12 +0200

    Migrated code to 1.21.2
    Signed-off-by: Eliah Lohr <lohr.eliah@gmail.com>

--- src/main/java/net/leloomi/vanillarice/item/ModFoodComponents.java
+++ src/main/java/net/leloomi/vanillarice/item/ModFoodComponents.java
@@ -1,18 +1,12 @@
 package net.leloomi.vanillarice.item;
 
 import net.minecraft.component.type.FoodComponent;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.registry.Registries;
-import net.minecraft.util.Identifier;
 
-
-public class ModFoodComponents
+public final class ModFoodComponents
 {
     public static final FoodComponent RICE_BOWL = new FoodComponent.Builder().
             nutrition(3)
             .saturationModifier(0.6F)
-            .usingConvertsTo(Registries.ITEM.get(Identifier.of("bowl")))
             .build();
 
     public static final FoodComponent MAKI_SUSHI = new FoodComponent.Builder()
@@ -23,14 +17,11 @@ public class ModFoodComponents
     public static final FoodComponent FRIED_RICE = new FoodComponent.Builder()
             .nutrition(5)
             .saturationModifier(0.6F)
-            .usingConvertsTo(Registries.ITEM.get(Identifier.of("bowl")))
             .build();
 
     public static final FoodComponent MOCHI = new FoodComponent.Builder()
-            .nutrition(2).saturationModifier(0.3F)
-            .statusEffect(
-                    new StatusEffectInstance(StatusEffects.REGENERATION, 60),
-                    1f)
+            .nutrition(2)
+            .saturationModifier(0.3F)
             .alwaysEdible()
             .build();
 }
--- src/main/java/net/leloomi/vanillarice/item/ModItems.java
+++ src/main/java/net/leloomi/vanillarice/item/ModItems.java
@@ -2,42 +2,77 @@ package net.leloomi.vanillarice.item;
 
 import net.leloomi.vanillarice.VanillaRice;
 import net.leloomi.vanillarice.block.ModBlocks;
-import net.minecraft.item.AliasedBlockItem;
+import net.minecraft.component.type.ConsumableComponents;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
+import net.minecraft.item.Items;
+import net.minecraft.item.consume.ApplyEffectsConsumeEffect;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
-public class ModItems
+import java.util.List;
+import java.util.function.Function;
+
+public final class ModItems
 {
-    public static final Item RICE_SEEDS = registerItem("rice_seeds",
-            new AliasedBlockItem(ModBlocks.RICE_CROP, new Item.Settings()));
-    
-    public static final Item RICE_GRAIN = registerItem("rice_grain",
-            new Item(new Item.Settings()));
+    public static final Item RICE_SEEDS = registerItem(
+            "rice_seeds",
+            settings -> new BlockItem(ModBlocks.RICE_CROP, settings),
+            new Item.Settings().useItemPrefixedTranslationKey());
 
-    public static final Item RICE_BOWL = registerItem("rice_bowl",
-            new Item(new Item.Settings().food(ModFoodComponents.RICE_BOWL)));
+    public static final Item RICE_GRAIN = registerItem(
+            "rice_grain",
+            Item::new,
+            new Item.Settings()
+    );
 
-    public static final Item MAKI_SUSHI = registerItem("maki_sushi",
-            new Item(new Item.Settings().food(ModFoodComponents.MAKI_SUSHI)));
+    public static final Item RICE_BOWL = registerItem(
+            "rice_bowl",
+            StewItem::new,
+            new StewItem.Settings().food(ModFoodComponents.RICE_BOWL)
+    );
 
-    public static final Item FRIED_RICE = registerItem("fried_rice",
-            new Item(new Item.Settings().food(ModFoodComponents.FRIED_RICE)));
+    public static final Item MAKI_SUSHI = registerItem(
+            "maki_sushi",
+            Item::new,
+            new Item.Settings().food(ModFoodComponents.MAKI_SUSHI)
+    );
 
-    public static final Item MOCHI = registerItem("mochi",
-            new Item(new Item.Settings().food(ModFoodComponents.MOCHI)));
+    public static final Item FRIED_RICE = registerItem(
+            "fried_rice",
+            StewItem::new,
+            new StewItem.Settings().food(ModFoodComponents.FRIED_RICE)
+    );
 
-    public static final Item RICE_BAG = registerItem("rice_bag",
-            new Item(new Item.Settings()));
+    public static final Item MOCHI = registerItem(
+            "mochi",
+            Item::new,
+            new Item.Settings()
+                    .food(ModFoodComponents.MOCHI, ConsumableComponents.food()
+                            .consumeEffect(new ApplyEffectsConsumeEffect(List.of(
+                                    new StatusEffectInstance(StatusEffects.REGENERATION,50,1),
+                                    new StatusEffectInstance(StatusEffects.RESISTANCE,100,1)
+                                    ))
+                            ).build()
+                    )
+    );
+
+    public static final Item RICE_BAG = registerItem(
+            "rice_bag",
+            Item::new,
+            new Item.Settings()
+    );
 
 
-    private static Item registerItem(String name, Item item)
-    {
-        return Registry.register(Registries.ITEM, Identifier.of(VanillaRice.MOD_ID, name), item);
+    public static Item registerItem(String path, Function<Item.Settings, Item> factory, Item.Settings settings) {
+        final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(VanillaRice.MOD_ID, path));
+        return Items.register(registryKey, factory, settings);
     }
 
-    public static void registerModItems()
+    public static void initialize()
     {
         VanillaRice.LOGGER.debug(VanillaRice.MOD_ID + ": Registering Items...");
     }
