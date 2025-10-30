c3ab65f6fca5d37bc279cca7d84f2defe997dd2a
Eliah Lohr <lohr.eliah@gmail.com>
2025-10-30 22:07:20 +0100

    Upgraded mod to 1.21.6

--- src/main/java/net/leloomi/vanillarice/VanillaRiceClient.java
+++ src/main/java/net/leloomi/vanillarice/VanillaRiceClient.java
@@ -1,15 +1,15 @@
 package net.leloomi.vanillarice;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
 import net.leloomi.vanillarice.block.ModBlocks;
-import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.BlockRenderLayer;
 
 public class VanillaRiceClient implements ClientModInitializer
 {
     @Override
     public void onInitializeClient()
     {
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.RICE_CROP, RenderLayer.getCutout());
+        BlockRenderLayerMap.putBlock(ModBlocks.RICE_CROP, BlockRenderLayer.CUTOUT);
     }
 }
